dc7bab9cb73630132212e127bdb908f434c9a7cf
Handsome Mofokeng <handsomem23@hotmail.com>
2021-01-27 02:33:02 +0200

    added validation code

--- lib/core/viewmodels/home_model.dart
+++ lib/core/viewmodels/home_model.dart
@@ -1,4 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class HomeModel extends ChangeNotifier {}
+class HomeModel extends ChangeNotifier {
+  bool _isVisible = false;
+  bool _isValid = false;
+
+  get isVisible => _isVisible;
+
+  set isVisible(value) {
+    _isVisible = value;
+    notifyListeners();
+  }
+
+  get isValid => _isValid;
+
+  void isValidEmail(String email) {
+    RegExp emailRegExp = new RegExp(
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+
+    if (emailRegExp.hasMatch(email)) {
+      _isValid = true;
+    } else {
+      _isValid = false;
+    }
+    notifyListeners();
+  }
+}
